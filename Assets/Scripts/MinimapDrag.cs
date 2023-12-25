081167247646b999983e8a0c69ce2b40278a54a7
CauBeLapTrinh <vuviet1402@gmail.com>
2023-12-25 13:00:38 +0700

    cmt

--- Assets/Scripts/MinimapDrag.cs
+++ Assets/Scripts/MinimapDrag.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
+using static UnityEditor.Progress;
 
 public class MinimapDrag : MonoBehaviour
 {
     public static MinimapDrag instance;
     public Camera cameraPos;
 
+    public float maxX;
+    public float minX;
+    public float maxY;
+    public float minY;
+
+
     Vector3 Origin;
     Vector3 Difference;
+    Vector3 lastDifference = Vector3.zero;
     Vector3 ResetCamera;
 
 
@@ -31,11 +39,84 @@ public class MinimapDrag : MonoBehaviour
         if (isDrag)
         {
             Difference = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
 
             Vector3 tem = Origin - Difference;
-            tem.z = -25;
 
-            cameraPos.transform.position = tem;
+            if (tem.x > 0 && tem.y > 0)
+            {
+                tem.x = 1f;
+                tem.y = 1f;
+            }
+            else if (tem.x <= 0 && tem.y <= 0)
+            {
+                tem.x = -1f;
+                tem.y = -1f;
+            }
+            if (tem.x > 0)
+            {
+                tem.x = 1f;
+            }
+            else if (tem.x <= 0)
+            {
+                tem.x = -1f;
+            }
+            if (tem.y > 0)
+            {
+                tem.y = 1f;
+            }
+            else if (tem.y <= 0)
+            {
+                tem.y = -1f;
+            }
+
+            Vector3 pos = cameraPos.transform.position + tem;
+
+            pos.z = -25;
+
+            if (pos.x >= maxX && pos.y >= maxY)
+            {
+                pos.x = maxX;
+                pos.y = maxY;
+            }
+            else if (pos.x <= minX && pos.y <= minY)
+            {
+                pos.x = minX;
+                pos.y = minY;
+            }
+            if (pos.x >= maxX && pos.y <= minY)
+            {
+                pos.x = maxX;
+                pos.y = minY;
+            }
+            else if (pos.x <= minX && pos.y >= maxY)
+            {
+                pos.x = minX;
+                pos.y = maxY;
+            }
+            if (pos.x >= maxX)
+            {
+                pos.x = maxX;
+            }
+            else if (pos.x <= minX)
+            {
+                pos.x = minX;
+            }
+            else if(pos.y >= maxY)
+            {
+                pos.y = maxY;
+            }
+            else if (pos.y <= minY)
+            {
+                pos.y = minY;
+            }
+
+            if(Difference != lastDifference)
+            {
+                cameraPos.transform.position = pos;
+            }
+
+            lastDifference = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
     }
 
@@ -44,13 +125,9 @@ public class MinimapDrag : MonoBehaviour
     { 
         if (ControlMiniMap.instance.isMiniMapOn)
         {
-            Origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            Debug.Log(Difference);
-
-            //Debug.Log(Vector2.Distance(Difference, cameraPos.transform.GetChild(0).position));
-
             isDrag = true;
+
+            Origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
         
     }
